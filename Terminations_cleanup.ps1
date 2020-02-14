9db28a505492315bc85e34fb4cfa697953f00db9
Steve Tollaksen <steve_tollaksen@SHAMROCKFOODS.com>
2020-02-14 10:53:36 -0700

    updated terminations to remove domain

--- Terminations_cleanup.ps1
+++ Terminations_cleanup.ps1
@@ -2,18 +2,18 @@
 #get rid of old stuff, make new folder structure
 
 
-$lostusers=Get-ADObject -SearchBase 'OU=Terminations,DC=Shamrockfoods,DC=COM' -filter {ObjectClass -ne "organizationalUnit"} -searchscope 1
+$lostusers=Get-ADObject -SearchBase 'OU=Terminations,DC=,DC=COM' -filter {ObjectClass -ne "organizationalUnit"} -searchscope 1
 $3months =  "{0:MM}" -f (get-date).AddMonths(3) + "01" +  "{0:yy}" -f (get-date).AddMonths(3) 
-$fullpath = "OU=$3months,OU=Terminations,DC=Shamrockfoods,DC=COM"
-if(!([adsi]::Exists("LDAP://$fullpath"))){New-ADOrganizationalUnit "$3months" -Path "OU=Terminations,DC=Shamrockfoods,DC=COM"}
+$fullpath = "OU=$3months,OU=Terminations,DC=,DC=COM"
+if(!([adsi]::Exists("LDAP://$fullpath"))){New-ADOrganizationalUnit "$3months" -Path "OU=Terminations,DC=,DC=COM"}
 
 foreach($user in $lostusers){
-    Move-ADObject -Identity $user.DistinguishedName -TargetPath  "OU=$3months,OU=Terminations,DC=Shamrockfoods,DC=COM"
+    Move-ADObject -Identity $user.DistinguishedName -TargetPath  "OU=$3months,OU=Terminations,DC=,DC=COM"
 }
 
 ############################################################################################################################
 
 $thismonth = "{0:MM}" -f (get-date) + "01" +  "{0:yy}" -f (get-date)
-$fullpath2 = "OU=$thismonth,OU=Terminations,DC=Shamrockfoods,DC=COM"
+$fullpath2 = "OU=$thismonth,OU=Terminations,DC=,DC=COM"
 $readytodelete = Get-ADObject -SearchBase $fullpath2  -filter *
 1..10 | % { foreach($object in $readytodelete){$object;Remove-ADObject $object -Confirm:$false }}
